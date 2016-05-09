a31ec127d5382d157a994498fe6eb16ff0b248c9
Joe Hillman <jhillman@lifeimage.com>
2016-05-09 07:37:24 -0400

    Strucutral - Added grunt sass plugin.

---
+++ grunt/sassTasks.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+  grunt.config('sass', {
+    sass: {
+      options: {
+        sourceMap: 'true'
+      }, //options
+      dist: {
+        files: {
+          'dist/css/main.css': 'src/scss/main.scss',
+        }
+      }
+    }
+  }) //config
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('compileSass', 'sass');
+}
