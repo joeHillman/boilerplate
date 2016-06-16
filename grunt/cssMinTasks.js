8b22113dc51ab3fcee9d80574bf49a4318e099be
Joe Hillman <jhillman@lifeimage.com>
2016-06-16 07:35:33 -0400

    Development - Adjusted minificiation path in preparation for packaging tasks.

--- grunt/cssMinTasks.js
+++ grunt/cssMinTasks.js
@@ -6,7 +6,7 @@ module.exports = function(grunt){
     target: {
       files: [{
         src: ['<%= src.build.css %>/main.expanded.css'],
-        dest: '<%= dest.dist.css %>/main.min.css'
+        dest: '<%= dest.build.css %>/main.min.css'
       }]
     }
   })// config
